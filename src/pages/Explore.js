09bc4f313a6a598ed9e56c9c31ae2020149755f2
yessGlory17 <ozgurkurucan337@gmail.com>
2022-01-02 00:04:34 +0300

    bug fix

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -3,10 +3,12 @@ import CardList from "../components/CardList";
 import { exploreList } from "../constants/MockupData";
 import '../styles/Explore.css';
 import Header from "../components/Header";
+import Search from "../components/Search";
 const Explore = () => {
   return (
     <div id="explore">
       <Header />
+      <Search/>
       <div id="list-container">
         <CardList list={exploreList} />
       </div>

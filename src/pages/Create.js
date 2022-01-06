d7f6b9420b4fb97f09584449e7e549d5b37074ec
yessGlory17 <ozgurkurucan337@gmail.com>
2022-01-07 01:23:30 +0300

    base button component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,15 @@
 import react from "react";
-
+import Card from "../components/base/Card";
+import TextInput from "../components/base/TextInput";
+import { Colors } from "../constants/Colors";
 const Create = () => {
 
 
     return (
         <div>
             Create Page
+            <Card blurColor={Colors.buttons.succes} />
+            <TextInput />
         </div>
     );
 }

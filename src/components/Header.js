3983214ee4d1a6030ba044eed4dcf10667570e40
bkaraceylan <burakkaraceylan@nara.com.tr>
2022-01-17 22:32:40 +0300

    walletlink, ledger, fortmatic, portis

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import react from "react";
 import { Link } from "react-router-dom";
 import { useEthers, useEtherBalance } from "@usedapp/core";
-import { walletConnect } from "../lib/wallets";
+import { walletConnect, walletLink, ledger, fortmatic, portis } from "../lib/wallets";
 
 const Header = () => {
 
@@ -16,8 +16,20 @@ const Header = () => {
         case "walletconnect":
           activate(walletConnect);
           break;
+        case "walletlink":
+            activate(walletLink);
+            break;
+        case "ledger":
+            activate(ledger);
+            break;
+        case "fortmatic":
+            activate(fortmatic);
+            break;
+        case "portis":
+            activate(portis);
+            break;
         default:
-          activateBrowserWallet();
+          console.log("Unknown wallet");
           break;
       }
     }
@@ -32,7 +44,7 @@ const Header = () => {
           <a>Community</a>
           <a>Craft NFT</a>
 
-          <button id="connect-wallet" onClick={connectWallet} >{!account ? 'Connect Wallet' : account}</button>
+          <button id="connect-wallet" onClick={() => connectWallet("metamask")} >{!account ? 'Connect Wallet' : account}</button>
         </div>
       </div>
     );
--- src/lib/wallets.js
+++ src/lib/wallets.js
@@ -1,18 +1,43 @@
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { LedgerConnector } from "@web3-react/ledger-connector";
+import { FortmaticConnector } from "@web3-react/fortmatic-connector";
+import { PortisConnector } from "@web3-react/portis-connector";
+
 const POLLING_INTERVAL = 12000;
 
 const RPC_URLS = {
-    1 : `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-    3 : `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-    4 : `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-    5 : `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-}
-console.log(RPC_URLS);
+  1: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  3: `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  4: `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  5: `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+};
 
 export const walletConnect = new WalletConnectConnector({
-    rpc: { 1: RPC_URLS[1], 3: RPC_URLS[3], 4: RPC_URLS[4], 5: RPC_URLS[5] },
-    bridge: 'https://bridge.walletconnect.org',
-    infuraId : process.env.REACT_APP_INFURA_KEY,
-    qrcode: true,
-    pollingInterval: POLLING_INTERVAL
-  })
+  rpc: { 1: RPC_URLS[1], 3: RPC_URLS[3], 4: RPC_URLS[4], 5: RPC_URLS[5] },
+  bridge: "https://bridge.walletconnect.org",
+  infuraId: process.env.REACT_APP_INFURA_KEY,
+  qrcode: true,
+  pollingInterval: POLLING_INTERVAL,
+});
+
+export const walletLink = new WalletLinkConnector({
+  url: RPC_URLS[1],
+  appName: "web3-react example",
+});
+
+export const ledger = new LedgerConnector({
+  chainId: 1,
+  url: RPC_URLS[1],
+  pollingInterval: POLLING_INTERVAL,
+});
+
+export const fortmatic = new FortmaticConnector({
+  apiKey: process.env.REACT_APP_FORMATIC_PROD_KEY,
+  chainId: 1,
+});
+
+export const portis = new PortisConnector({
+  dAppId: process.env.REACT_APP_PORTIS_DAPP_ID,
+  networks: [1, 100]
+});

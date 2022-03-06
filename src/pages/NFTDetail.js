276d0b49858b95406c5bce5a840bae570138a0e3
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-03-06 18:25:48 +0300

    Added Horizontal Scrollbar NFTDetail Page

--- src/pages/NFTDetail.js
+++ src/pages/NFTDetail.js
@@ -7,12 +7,15 @@ import "../styles/NFTDetail.css";
 import { ColorExtractor } from "react-color-extractor";
 import Button from "../components/base/Button";
 import { FaEthereum } from "react-icons/fa";
-import { AiOutlineHeart, AiFillHeart, AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import {
+  AiOutlineHeart,
+  AiFillHeart,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
 import { useMobile } from "../hooks/isMobile";
 import { hotDropsData } from "../constants/MockupData";
 import NFTCard from "../components/NFTCard";
-
-
 
 const NFTDetail = () => {
   const isMobile = useMobile();
@@ -20,7 +23,6 @@ const NFTDetail = () => {
   const [colors, setColors] = useState([]);
 
   const [isLike, setIsLike] = useState(false);
-
 
   const like = () => setIsLike(!isLike);
 
@@ -35,10 +37,6 @@ const NFTDetail = () => {
   useEffect(() => {
     setColors([]);
   }, [state]);
-
-
-
-  
 
   //!! aciklama karakter sayisi sinirlanmali.
   //!! scroll sorununa cozum bulunmali.
@@ -59,11 +57,10 @@ const NFTDetail = () => {
               </ColorExtractor>
 
               <div id="detail-info" style={{}}>
-                <div id='detail-info-container'>
+                <div id="detail-info-container">
                   <p id="collection"> {state.item.name} </p>
                   <p id="name"> {state.item.name} </p>
-                  <p id="description" > {state.item.description} </p>
-
+                  <p id="description"> {state.item.description} </p>
                 </div>
 
                 <div id="detail-controls">
@@ -102,9 +99,232 @@ const NFTDetail = () => {
             </div>
           }
         />
-        
       </div>
+      <div id="more-nft-list-wrapper">
+        {/* DECOY CARDS */}
+        <Card
+          width={isMobile ? "100%" : "65vw"}
+          height={isMobile ? "700px" : "60vh"}
+          blurColor={colors[0]}
+          child={
+            //Detail Content
+            <div id="detail-content">
+              <ColorExtractor getColors={getColors}>
+                <img id="detail-image" src={state.item.src} />
+              </ColorExtractor>
 
+              <div id="detail-info" style={{}}>
+                <div id="detail-info-container">
+                  <p id="collection"> {state.item.name} </p>
+                  <p id="name"> {state.item.name} </p>
+                  <p id="description"> {state.item.description} </p>
+                </div>
+
+                <div id="detail-controls">
+                  <Button
+                    width={isMobile ? "70%" : "70%"}
+                    height="50px"
+                    child={
+                      <div id="button-child">
+                        <FaEthereum size="28px" />
+                        <p id="price">1254</p>
+                      </div>
+                    }
+                  ></Button>
+                  <div className="like-container">
+                    <button className="like" onClick={like}>
+                      {!isLike ? (
+                        <AiOutlineHeart size="45" color="white" />
+                      ) : (
+                        <AiFillHeart
+                          size="45"
+                          style={{
+                            stroke: `-webkit-linear-gradient(
+                    to bottom,
+                    #38ef7d,
+                    #11998e
+                  );`,
+                          }}
+                          color="#00f5c966"
+                        />
+                      )}
+                    </button>
+                    <p className="like-count">123</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+
+        {/* DECOY CARD */}
+        <Card
+          width={isMobile ? "100%" : "65vw"}
+          height={isMobile ? "700px" : "60vh"}
+          blurColor={colors[0]}
+          child={
+            //Detail Content
+            <div id="detail-content">
+              <ColorExtractor getColors={getColors}>
+                <img id="detail-image" src={state.item.src} />
+              </ColorExtractor>
+
+              <div id="detail-info" style={{}}>
+                <div id="detail-info-container">
+                  <p id="collection"> {state.item.name} </p>
+                  <p id="name"> {state.item.name} </p>
+                  <p id="description"> {state.item.description} </p>
+                </div>
+
+                <div id="detail-controls">
+                  <Button
+                    width={isMobile ? "70%" : "70%"}
+                    height="50px"
+                    child={
+                      <div id="button-child">
+                        <FaEthereum size="28px" />
+                        <p id="price">1254</p>
+                      </div>
+                    }
+                  ></Button>
+                  <div className="like-container">
+                    <button className="like" onClick={like}>
+                      {!isLike ? (
+                        <AiOutlineHeart size="45" color="white" />
+                      ) : (
+                        <AiFillHeart
+                          size="45"
+                          style={{
+                            stroke: `-webkit-linear-gradient(
+                    to bottom,
+                    #38ef7d,
+                    #11998e
+                  );`,
+                          }}
+                          color="#00f5c966"
+                        />
+                      )}
+                    </button>
+                    <p className="like-count">123</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+
+        {/* DECOY CARDS */}
+        <Card
+          width={isMobile ? "100%" : "65vw"}
+          height={isMobile ? "700px" : "60vh"}
+          blurColor={colors[0]}
+          child={
+            //Detail Content
+            <div id="detail-content">
+              <ColorExtractor getColors={getColors}>
+                <img id="detail-image" src={state.item.src} />
+              </ColorExtractor>
+
+              <div id="detail-info" style={{}}>
+                <div id="detail-info-container">
+                  <p id="collection"> {state.item.name} </p>
+                  <p id="name"> {state.item.name} </p>
+                  <p id="description"> {state.item.description} </p>
+                </div>
+
+                <div id="detail-controls">
+                  <Button
+                    width={isMobile ? "70%" : "70%"}
+                    height="50px"
+                    child={
+                      <div id="button-child">
+                        <FaEthereum size="28px" />
+                        <p id="price">1254</p>
+                      </div>
+                    }
+                  ></Button>
+                  <div className="like-container">
+                    <button className="like" onClick={like}>
+                      {!isLike ? (
+                        <AiOutlineHeart size="45" color="white" />
+                      ) : (
+                        <AiFillHeart
+                          size="45"
+                          style={{
+                            stroke: `-webkit-linear-gradient(
+                    to bottom,
+                    #38ef7d,
+                    #11998e
+                  );`,
+                          }}
+                          color="#00f5c966"
+                        />
+                      )}
+                    </button>
+                    <p className="like-count">123</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+
+        {/* DECOY CARDS */}
+        <Card
+          width={isMobile ? "100%" : "65vw"}
+          height={isMobile ? "700px" : "60vh"}
+          blurColor={colors[0]}
+          child={
+            //Detail Content
+            <div id="detail-content">
+              <ColorExtractor getColors={getColors}>
+                <img id="detail-image" src={state.item.src} />
+              </ColorExtractor>
+
+              <div id="detail-info" style={{}}>
+                <div id="detail-info-container">
+                  <p id="collection"> {state.item.name} </p>
+                  <p id="name"> {state.item.name} </p>
+                  <p id="description"> {state.item.description} </p>
+                </div>
+
+                <div id="detail-controls">
+                  <Button
+                    width={isMobile ? "70%" : "70%"}
+                    height="50px"
+                    child={
+                      <div id="button-child">
+                        <FaEthereum size="28px" />
+                        <p id="price">1254</p>
+                      </div>
+                    }
+                  ></Button>
+                  <div className="like-container">
+                    <button className="like" onClick={like}>
+                      {!isLike ? (
+                        <AiOutlineHeart size="45" color="white" />
+                      ) : (
+                        <AiFillHeart
+                          size="45"
+                          style={{
+                            stroke: `-webkit-linear-gradient(
+                    to bottom,
+                    #38ef7d,
+                    #11998e
+                  );`,
+                          }}
+                          color="#00f5c966"
+                        />
+                      )}
+                    </button>
+                    <p className="like-count">123</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+      </div>
     </div>
   );
 };

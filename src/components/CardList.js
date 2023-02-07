b49c7a341e140711be14c83cd1749e937ed30577
Vansh Baghel <vanshbaghel912@gmail.com>
2023-02-07 21:28:42 +0530

    Issue #39. Added carousel
    * Added responsive carousel.
    * Showing multiple NFTs for different screen sizes.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,15 +2,47 @@ import React from "react";
 import NFTCard from "./NFTCard";
 import "../styles/CardList.css";
 import { useNavigate } from "react-router-dom";
+import Carousel from "react-elastic-carousel";
+import { useRef } from "react";
 
-const CardList = ({ list,type="horizontal" }) => {
+const CardList = ({ list, type = "horizontal" }) => {
   let navigate = useNavigate();
+  const carouselRef = useRef(null);
+  let resetTimeout;
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
 
   return (
-    <div id="card-list" style={{flexDirection:type=="horizontal" ? "row" : "column"}}>
-      {list.map((item,index) => (
-        <NFTCard nftSrc={item.src} key={index} onClick={()=>navigate('/detail',{state:{item:item}})}/>
-      ))}
+    <div
+      id="card-list"
+      style={{ flexDirection: type == "horizontal" ? "row" : "column" }}
+    >
+      <Carousel
+        ref={carouselRef}
+        enableAutoPlay={true}
+        autoPlaySpeed={4000}
+        isRTL={false}
+        breakPoints={breakPoints}
+        onNextEnd={({ index }) => {
+          clearTimeout(resetTimeout);
+          resetTimeout = setTimeout(() => {
+            carouselRef?.current?.goTo(0);
+          }, 4000); // same time
+        }}
+      >
+        {list.map((item, index) => (
+          <NFTCard
+            nftSrc={item.src}
+            key={index}
+            onClick={() => navigate("/detail", { state: { item: item } })}
+          />
+        ))}
+      </Carousel>
     </div>
   );
 };

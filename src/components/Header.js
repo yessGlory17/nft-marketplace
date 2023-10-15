26889a276796ae2348b4a1ed1fd91e1ee813e77c
shawn <shawnpereiraswp@gmail.com>
2023-10-15 21:55:56 +0530

    code cleant, added auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,29 +3,32 @@ import { Link } from "react-router-dom";
 import { useEthers, useEtherBalance } from "@usedapp/core";
 
 const Header = () => {
+  const { activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
 
-    const {activateBrowserWallet, account} = useEthers();
-    const etherBalance = useEtherBalance(account);
+  const handleWallet = () => {
+    activateBrowserWallet();
+  };
 
-    const handleWallet = () => {
-      activateBrowserWallet();
+  return (
+    <div id="header">
+      <Link to="/" id="logo">
+        NFT Room
+      </Link>
+      <h1 style={{ color: "white" }}>Hello</h1>
 
-    }
+      <div id="link-containers">
+        <a>Start Hunting</a>
+        <a>Dark NFTs</a>
+        <a>Community</a>
+        <a>Craft NFT</a>
 
-    return (
-        <div id="header">
-        <Link to='/' id='logo'>NFT Room</Link>
-
-        <div id="link-containers">
-          <a>Start Hunting</a>
-          <a>Dark NFTs</a>
-          <a>Community</a>
-          <a>Craft NFT</a>
-
-          <button id="connect-wallet" onClick={handleWallet} >{!account ? 'Connect Wallet' : account}</button>
-        </div>
+        <button id="connect-wallet" onClick={handleWallet}>
+          {!account ? "Connect Wallet" : account}
+        </button>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Header;
--- src/components/NFTCard.js
+++ src/components/NFTCard.js
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from "react";
 import "../styles/NFTCard.css";
 import { FaEthereum } from "react-icons/fa";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { ColorExtractor } from 'react-color-extractor'
+import { ColorExtractor } from "react-color-extractor";
 import Card from "./base/Card";
 import Button from "./base/Button";
 import { Colors } from "../constants/Colors";
 
-import { ModelViewerElement } from "@google/model-viewer";
+// import { ModelViewerElement } from "@google/model-viewer";
 import { useARStatus } from "../hooks/isARStatus";
 
-
-
-const NFTCard = ({ username, nftName, price, nftSrc, likeCount, gradient, onClick }) => {
+const NFTCard = ({
+  username,
+  nftName,
+  price,
+  nftSrc,
+  likeCount,
+  gradient,
+  onClick,
+}) => {
   const [isLike, setIsLike] = useState(false);
   const [colors, setColors] = useState([]);
 
@@ -20,62 +26,86 @@ const NFTCard = ({ username, nftName, price, nftSrc, likeCount, gradient, onClic
 
   useEffect(() => {
     console.log(isARSupport);
-  }, [])
+  }, []);
 
   const like = () => setIsLike(!isLike);
 
-  const getColors = colors => {
-    setColors(c => [...c, ...colors]);
+  const getColors = (colors) => {
+    setColors((c) => [...c, ...colors]);
     //console.log(colors);
-  }
+  };
 
   return (
     <Card
       blurColor={colors[0]}
-
-      child={<>
-        {isARSupport ? <model-viewer ar-scale="auto" ar ar-modes="webxr scene-viewer quick-look" id="reveal" loading="eager" camera-controls auto-rotate src={nftSrc} > </model-viewer> : <><ColorExtractor getColors={getColors}>
-          <img className="nft-image" src={nftSrc} />
-        </ColorExtractor></>}
-        <div className="wrapper">
-          <div className="info-container">
-            <p className="owner"> LEJOURN.DARK.NFT</p>
-            <p className="name">Alien Cry</p>
-          </div>
-
-          <div className="price-container">
-            <p className="price-label">Price</p>
-            <p className="price">
+      child={
+        <>
+          {isARSupport ? (
+            <model-viewer
+              ar-scale="auto"
+              ar
+              ar-modes="webxr scene-viewer quick-look"
+              id="reveal"
+              loading="eager"
+              camera-controls
+              auto-rotate
+              src={nftSrc}
+            >
               {" "}
-              <FaEthereum /> 4.555
-            </p>
+            </model-viewer>
+          ) : (
+            <>
+              <ColorExtractor getColors={getColors}>
+                <img className="nft-image" src={nftSrc} />
+              </ColorExtractor>
+            </>
+          )}
+          <div className="wrapper">
+            <div className="info-container">
+              <p className="owner"> LEJOURN.DARK.NFT</p>
+              <p className="name">Alien Cry</p>
+            </div>
+
+            <div className="price-container">
+              <p className="price-label">Price</p>
+              <p className="price">
+                {" "}
+                <FaEthereum /> 4.555
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="buttons">
-          {/* <button className="buy-now">Buy Now</button> */}
-          <Button color={Colors.buttons.primary} textContent="Buy Now" onClick={onClick} />
-          <div className="like-container">
-            <button className="like" onClick={like}>
-              {!isLike ? (
-                <AiOutlineHeart size="30" color="white" />
-              ) : (
-                <AiFillHeart size="30" style={{
-                  stroke: `-webkit-linear-gradient(
+          <div className="buttons">
+            {/* <button className="buy-now">Buy Now</button> */}
+            <Button
+              color={Colors.buttons.primary}
+              textContent="Buy Now"
+              onClick={onClick}
+            />
+            <div className="like-container">
+              <button className="like" onClick={like}>
+                {!isLike ? (
+                  <AiOutlineHeart size="30" color="white" />
+                ) : (
+                  <AiFillHeart
+                    size="30"
+                    style={{
+                      stroke: `-webkit-linear-gradient(
                     to bottom,
                     #38ef7d,
                     #11998e
-                  );`
-                }} color='#00f5c966' />
-              )}
-            </button>
-            <p className="like-count">123</p>
+                  );`,
+                    }}
+                    color="#00f5c966"
+                  />
+                )}
+              </button>
+              <p className="like-count">123</p>
+            </div>
           </div>
-        </div>
-      </>}>
-
-    </Card>
+        </>
+      }
+    ></Card>
   );
 };
 
 export default NFTCard;
-
